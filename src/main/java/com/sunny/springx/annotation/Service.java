1de2a7651fc83a6972a8d5dd418da0a0a3605d5f
zhaoyunxing92 <2385585770@qq.com>
2018-12-31 02:31:51 +0800

    添加dispatchServlet

---
+++ src/main/java/com/sunny/springx/annotation/Service.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing92 Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.springx.annotation;
+
+/**
+ * @author zhaoyunxing92
+ * @date: 2018-12-31 01:50
+ * @des:
+ */
+public @interface Service {
+}
---
+++ src/main/java/com/sunny/springx/webmvc/servlet/DispatcherServlet.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing92 Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.springx.webmvc.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author zhaoyunxing92
+ * @date: 2018-12-31 02:18
+ * @des: 处理全部请求
+ */
+public class DispatcherServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
