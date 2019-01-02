dd657ac915b0a40c626018bf89ebb025595dcdc4
zhaoyunxing92 <2385585770@qq.com>
2019-01-02 09:46:08 +0800

    添加注释

--- src/main/java/com/sunny/springx/util/StringUtils.java
+++ src/main/java/com/sunny/springx/util/StringUtils.java
@@ -25,19 +25,23 @@ public class StringUtils {
     }
 
     /**
-     * 首字母小写
+     * 首字母小写，首个字符不是字母或者不是小写的字母不处理
      *
      * @param str 字母字符串 A-65 a-97
      * @return 小写后的字符串  null则返回""
      */
     public static String lowerFirstCase(String str) {
-        if (isBlank(str)) return "";
+        if (isBlank(str)) {
+            return "";
+        }
 
         char[] chars = str.toCharArray();
 
         int value = (int) chars[0];
-        //首个不是字母或者不是小写的字母不处理
-        if (value < 65 || value > 91) return str;
+        //首个字符不是字母或者不是小写的字母不处理
+        if (value < 65 || value > 91) {
+            return str;
+        }
 
         chars[0] += 32;
         return String.valueOf(chars);
