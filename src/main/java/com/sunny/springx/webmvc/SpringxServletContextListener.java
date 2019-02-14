18c81ff71a1a0ccaa06e37dc532b43b3ae9c8ba8
zhaoyunxing92 <2385585770@qq.com>
2019-02-14 21:34:21 +0800

    [up]添加spi机制

--- src/main/java/com/sunny/springx/webmvc/SpringxServletContextListener.java
+++ src/main/java/com/sunny/springx/webmvc/SpringxServletContextListener.java
@@ -14,28 +14,14 @@ import java.util.Set;
 /**
  * @author sunny
  * @date: 2019-02-13 00:54
- * @des: 监听容器初始化
+ * @des: 监听容器初始化 spi机制
  */
-//@WebListener
-public class SpringxServletContextListener  implements ServletContainerInitializer {
+public class SpringxServletContextListener implements ServletContainerInitializer {
     @Override
     public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
         ServletRegistration.Dynamic dispatcherServlet = ctx.addServlet("dispatcherServlet", DispatcherServlet.class);
+        dispatcherServlet.setInitParameter("scanPackage", "com.sunny.springx.example");
+        dispatcherServlet.addMapping("/*");
+        dispatcherServlet.setLoadOnStartup(1);
     }
-
-    //    @Override
-//    public void contextInitialized(ServletContextEvent sce) {
-//        //动态注册　dispatcherServlet
-//        ServletContext servletContext = sce.getServletContext();
-//        ServletRegistration.Dynamic dispatcherServlet = servletContext.addServlet("dispatcherServlet", DispatcherServlet.class);
-//        //指定扫描的包
-//        dispatcherServlet.setInitParameter("scanPackage", "com.sunny.springx.example");
-//        dispatcherServlet.addMapping("/*");
-//        dispatcherServlet.setLoadOnStartup(1);
-//    }
-//
-//    @Override
-//    public void contextDestroyed(ServletContextEvent sce) {
-//
-//    }
 }
